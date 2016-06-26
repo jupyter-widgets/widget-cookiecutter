3ba0404be410bceda07bb38fbabd4c326a0ac760
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-06-26 19:42:22 -0400

    Fix webpack public path template

--- {{cookiecutter.github_project_name}}/js/src/index.js
+++ {{cookiecutter.github_project_name}}/js/src/index.js
@@ -5,9 +5,8 @@
 // Some static assets may be required by the custom widget javascript. The base
 // url for the notebook is not known at build time and is therefore computed
 // dynamically.
-__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/example/';
+__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/{{ cookiecutter.npm_package_name }}/';
 
-
-// Export everything from example and the npm package version number.
+// Export widget models and views, and the npm package version number.
 module.exports = require('./example.js');
 module.exports['version'] = require('../package.json').version;
