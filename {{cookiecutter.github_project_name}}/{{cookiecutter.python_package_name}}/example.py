7de60a275708d4fd1269b0cca7fac4bb0fa2a452
Pascal Bugnion <pascal@bugnion.org>
2020-01-11 11:00:36 +0000

    Use comments instead of multi-line strings
    There is no programmatic meaning to these comments, so we shouldn't
    use strings.

--- {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/example.py
+++ {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/example.py
@@ -1,26 +1,32 @@
 import ipywidgets as widgets
-""" Widget properties are defined as traitlets. This allows automatic
-syncing of properties between kernel and front-end. These properties are
-accessible in front-end using model.get('property name') with use of
-Backbone.js based widget front-end models.
-"""
+""
 from traitlets import Unicode
 
 """ Declare widget class and register to widget registry """
 @widgets.register
 class HelloWorld(widgets.DOMWidget):
     """An example widget."""
-    """ Name of the widget view class in front-end """
+
+    # Name of the widget view class in front-end
     _view_name = Unicode('HelloView').tag(sync=True)
-    """ Name of the widget model class in front-end """
+
+    # Name of the widget model class in front-end
     _model_name = Unicode('HelloModel').tag(sync=True)
-    """ Name of the front-end module containing widget view """
+
+    # Name of the front-end module containing widget view
     _view_module = Unicode('{{ cookiecutter.npm_package_name }}').tag(sync=True)
-    """ Name of the front-end module containing widget model """
+
+    # Name of the front-end module containing widget model
     _model_module = Unicode('{{ cookiecutter.npm_package_name }}').tag(sync=True)
-    """ Version of the front-end module containing widget view """
+
+    # Version of the front-end module containing widget view
     _view_module_version = Unicode('^{{ cookiecutter.npm_package_version }}').tag(sync=True)
-    """ Version of the front-end module containing widget model """
+    # Version of the front-end module containing widget model
     _model_module_version = Unicode('^{{ cookiecutter.npm_package_version }}').tag(sync=True)
-    """ Widget specific property """
+
+    # Widget specific property.
+    # Widget properties are defined as traitlets. Any property tagged with `sync=True`
+    # is automatically synced to the frontend *any* time it changes. It is
+    # accessible from the frontend model as `this.get("value")` and from the
+    # frontend view as `this.model.get("value")`.
     value = Unicode('Hello World!').tag(sync=True)
