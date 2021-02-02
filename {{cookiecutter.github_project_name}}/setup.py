299f323bf1956fdf99023b060bb387589c1cc7eb
Jason Grout <jgrout6@bloomberg.net>
2021-02-01 16:16:00 -0800

    Update packaging to use more modern python practices

--- {{cookiecutter.github_project_name}}/setup.py
+++ {{cookiecutter.github_project_name}}/setup.py
@@ -1,36 +1,19 @@
-from __future__ import print_function
-from setuptools import setup, find_packages
-import os
-from os.path import join as pjoin
-from distutils import log
+from setuptools import setup
+from os import path
 
 from jupyter_packaging import (
     create_cmdclass,
     install_npm,
     ensure_targets,
     combine_commands,
-    get_version,
 )
 
+HERE = path.dirname(path.abspath(__file__))
 
-here = os.path.dirname(os.path.abspath(__file__))
-
-log.set_verbosity(log.DEBUG)
-log.info('setup.py entered')
-log.info('$PATH=%s' % os.environ['PATH'])
-
-name = '{{ cookiecutter.python_package_name  }}'
-LONG_DESCRIPTION = '{{ cookiecutter.project_short_description }}'
-
-# Get {{ cookiecutter.python_package_name  }} version
-version = get_version(pjoin(name, '_version.py'))
-
-js_dir = pjoin(here, 'js')
+js_dir = path.join(HERE, 'js')
 
 # Representative files that should exist after a successful build
-jstargets = [
-    pjoin(js_dir, 'dist', 'index.js'),
-]
+jstargets = [path.join(js_dir, 'dist', 'index.js')]
 
 data_files_spec = [
     ('share/jupyter/nbextensions/{{ cookiecutter.npm_package_name }}', '{{ cookiecutter.python_package_name }}/nbextension', '*.*'),
@@ -41,40 +24,8 @@ data_files_spec = [
 
 cmdclass = create_cmdclass('jsdeps', data_files_spec=data_files_spec)
 cmdclass['jsdeps'] = combine_commands(
-    install_npm(js_dir, npm=['yarn'], build_cmd='build:prod'), ensure_targets(jstargets),
+    install_npm(js_dir, npm=['yarn'], build_cmd='build:prod'),
+    ensure_targets(jstargets),
 )
 
-setup_args = dict(
-    name=name,
-    version=version,
-    description='{{ cookiecutter.project_short_description }}',
-    long_description=LONG_DESCRIPTION,
-    include_package_data=True,
-    install_requires=[
-        'ipywidgets>=7.6.0',
-    ],
-    packages=find_packages(),
-    zip_safe=False,
-    cmdclass=cmdclass,
-    author='{{ cookiecutter.author_name }}',
-    author_email='{{ cookiecutter.author_email }}',
-    url='https://github.com/{{ cookiecutter.github_organization_name  }}/{{ cookiecutter.github_project_name }}',
-    keywords=[
-        'ipython',
-        'jupyter',
-        'widgets',
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: IPython',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-)
-
-setup(**setup_args)
+setup(cmdclass=cmdclass)
